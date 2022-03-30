7224c60b66aeb4895065137dc7a2210d6b5970ff
hallihk <hallvard@hiof.no>
2022-03-30 03:29:53 +0200

    final glemte noe

--- src/lib/client.js
+++ src/lib/client.js
@@ -4,6 +4,7 @@ const options = {
     projectId: "idh615gy",
     dataset: "production",
     apiVersion: '2021-08-31',
+    token: process.env.REACT_APP_SANITY_TOKEN,
 }
 const client = sanityClient({
     ...options,
